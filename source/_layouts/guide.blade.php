3c06b689b4bd26ebfe1dabc25383a0743329aeb0
Luke Bouch <lukebouch2014@gmail.com>
2021-12-07 15:07:24 -0500

    Center date

--- source/_layouts/guide.blade.php
+++ source/_layouts/guide.blade.php
@@ -2,5 +2,5 @@
 
 @section('body')
 <h1 class="text-center max-w-3xl mx-auto">{{ $page->title }}</h1>
-<p>{{ date('F j, Y', $page->date) }}</p>
+<p class="text-center">{{ date('F j, Y', $page->date) }}</p>
 @endsection
