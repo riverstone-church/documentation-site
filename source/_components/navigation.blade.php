7fcb0cbcd13b7ef15fcbe76a274f0f5ad6a57906
Luke Bouch <lukebouch2014@gmail.com>
2021-12-07 14:05:30 -0500

    Add basic layout

---
+++ source/_components/navigation.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <div class="container m-4">
+        <a class="flex items-center text-xl font-bold" href="/">
+            <x-icon class="w-12 h-12 mr-4" />
+            Resources
+        </a>
+    </div>
+</nav>
--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
-        <meta name="description" content="{{ $page->description }}">
-        <title>{{ $page->title }}</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
-    </head>
-    <body class="text-gray-900 font-sans antialiased">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="{{ $page->getUrl() }}">
+    <meta name="description" content="{{ $page->description }}">
+    <title>{{ $page->title }}</title>
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+</head>
+
+<body class="text-gray-900 font-sans antialiased">
+    <header>
+        <x-navigation />
+    </header>
+    <main class="container">
         @yield('body')
-    </body>
+    </main>
+</body>
+
 </html>
