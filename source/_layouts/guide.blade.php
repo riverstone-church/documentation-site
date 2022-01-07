8c753ad97da41b8fa7281e4f811c4ea07fcb13ba
Luke Bouch <lukebouch2014@gmail.com>
2022-01-07 15:18:48 -0500

    Improve next and previous links

--- source/_layouts/guide.blade.php
+++ source/_layouts/guide.blade.php
@@ -10,23 +10,27 @@
 <p class="text-center">{{ date('F j, Y', $page->date) }}</p>
 @endsection
 @section('after')
-<div class="container grid md:grid-cols-2 mt-5 mb-4">
+<div class="max-w-xl mx-auto grid md:grid-cols-2 mt-10 mb-7 gap-3">
     @if($page->getPrevious())
-    <a class="text-right" href="{{ optional($page->getPrevious())->getUrl() }}">
-        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-        </svg>
+    <a class="flex items-center" href="{{ optional($page->getPrevious())->getUrl() }}">
+        <div class="mr-3">
+            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+        </div>
         {{ optional($page->getPrevious())->title }}
     </a>
     @endif
     @if($page->getNext())
-    <a class="col-start-2" href="{{ optional($page->getNext())->getUrl() }}">
+    <a class="flex items-center text-right col-start-2" href="{{ optional($page->getNext())->getUrl() }}">
         {{ optional($page->getNext())->title }}
-        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-        </svg>
+        <div class="ml-3">
+            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+        </div>
     </a>
     @endif
 </div>
