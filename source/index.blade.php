b1466b3e26dc697bf24d3020c7ce4b1c285ed51b
Luke Bouch <lukebouch2014@gmail.com>
2022-02-10 11:09:13 -0500

    Improve link layout on homepage

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,42 +9,44 @@
 <section class="my-10">
     <x-anchor href="/branding">Logos & Branding</x-anchor>
 </section>
-<section class="pb-8 mt-10">
-    <h2>Guides</h2>
-    <ul class="pt-3">
-        @foreach ($guides as $guide)
-        <li class="max-w-xl h-full">
-            <a href="{{ $guide->getPath() }}">
-                <h4>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                    </svg>
-                    {{ $guide->title }}
-                </h4>
-            </a>
-        </li>
-        @endforeach
-    </ul>
-</section>
-<section class="pb-8">
-    <h2>Documentation</h2>
-    <ul class="pt-3">
-        @foreach ($docs as $doc)
-        <li class="max-w-xl h-full">
-            <a href="{{ $doc->getPath() }}">
-                <h4>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                    </svg>
-                    {{ $doc->title }}
-                </h4>
-            </a>
-        </li>
-        @endforeach
-    </ul>
+<section class="grid md:grid-cols-2 gap-5 pb-8 my-10">
+    <div>
+        <h2>Guides</h2>
+        <ul class="pt-3">
+            @foreach ($guides as $guide)
+            <li class="max-w-xl h-full ml-8">
+                <a href="{{ $guide->getPath() }}">
+                    <h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1 -ml-8" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                        </svg>
+                        {{ $guide->title }}
+                    </h4>
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    <div>
+        <h2>Documentation</h2>
+        <ul class="pt-3">
+            @foreach ($docs as $doc)
+            <li class="max-w-xl h-full">
+                <a href="{{ $doc->getPath() }}">
+                    <h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6 -mt-1" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                        </svg>
+                        {{ $doc->title }}
+                    </h4>
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </section>
 @endsection
