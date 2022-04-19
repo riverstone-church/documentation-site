fdab585d65bd4f5f15d467eb2d3b3759ff3866c4
Luke Bouch <lukebouch2014@gmail.com>
2022-04-19 11:16:15 -0400

    Update guide.blade.php

--- source/_layouts/guide.blade.php
+++ source/_layouts/guide.blade.php
@@ -10,14 +10,14 @@
         max-width: 150px;
     }
 </style>
-<div class="max-w-xl mx-auto text-center print:mt-10 print:text-left">
+<div class="max-w-xl mx-auto text-center print:mt-10 print:mb-5 print:text-left">
     @if($page->image)
     <img class="mx-auto mb-5 featuredImage" src="{{ $page->image }}">
     @endif
     <h1 class="mx-auto print:ml-0">{{ $page->title }}</h1>
     <p>{{ date('F j, Y', $page->date) }}</p>
     <p class="-mt-3">{{ $page->author ?? '' }}</p>
-    <a class="hidden print:inline-block" href="{{ $page->getUrl() }}">{{ $page->getUrl() }}</a>
+    <a class="hidden print:inline-block underline" href="{{ $page->getUrl() }}">{{ $page->getUrl() }}</a>
 </div>
 @endsection
 @section('after')
