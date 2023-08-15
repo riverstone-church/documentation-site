395dd0c82c24da8e7a25c38c1926ff23ef23521d
Luke Bouch <luke@lukebouch.com>
2023-08-15 18:52:47 -0400

    Start fresh

--- source/_components/navigation.blade.php
+++
@@ -1,8 +0,0 @@
-<nav class="print:hidden">
-    <div class="container m-4">
-        <a class="flex items-center text-xl font-bold" href="/">
-            <x-icon class="w-12 h-12 mr-4" />
-            RiverStone Church
-        </a>
-    </div>
-</nav>
